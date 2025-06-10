b4948b79fd0724395e3ad3b8fcb1af6d417bf900
Nicolas <nicolasmarroyoz@gmail.com>
2025-06-10 12:53:10 -0300

    punto 3 completado

--- Program.cs
+++ Program.cs
@@ -32,6 +32,88 @@ else
 
 
 //Ejercicio 2 y 3
+float solicitarNumero()
+{
+    string numString = Console.ReadLine();
+    float num;
+
+    while (!float.TryParse(numString, out num))
+    {
+        Console.Write("Ingrese un numero valido");
+        numString = Console.ReadLine();
+    }
+    return num;
+}
+
+float suma(float num1, float num2)
+{
+    return num1 + num2;
+}
+float resta(float num1, float num2)
+{
+    return num1 - num2;
+}
+float multiplicacion(float num1, float num2)
+{
+    return num1 * num2;
+}
+float division(float num1, float num2)
+{
+    return num1 / num2;
+}
+
+string opcionString;
+int opcionInt = 0;
+float num1, num2;
+
+while (opcionInt != 5)
+{
+    Console.WriteLine("\n================================\n");
+    Console.WriteLine("Seleccione una opcion:");
+    Console.WriteLine("1. Sumar");
+    Console.WriteLine("2. Restar");
+    Console.WriteLine("3. Multiplicar");
+    Console.WriteLine("4. Dividir");
+    Console.WriteLine("5. Salir");
+
+    opcionString = Console.ReadLine();
+
+    bool opcionValida = int.TryParse(opcionString, out opcionInt);
+    Console.WriteLine("\n------------------------\n");
+    if (opcionValida && opcionInt > 0 && opcionInt < 5)
+    {
+        Console.WriteLine("Ingrese el primer numero:");
+        num1 = solicitarNumero();
+        Console.WriteLine("\nIngrese el segundo numero");
+        num2 = solicitarNumero();
+        Console.WriteLine("\n------------------------\n");
+        switch (opcionInt)
+        {
+            case 1:
+                Console.WriteLine(num1 + " + " + num2 + " = " + suma(num1, num2));
+                break;
+            case 2:
+                Console.WriteLine(num1 + " - " + num2 + " = " + resta(num1, num2));
+                break;
+            case 3:
+                Console.WriteLine(num1 + " * " + num2 + " = " + multiplicacion(num1, num2));
+                break;
+            case 4:
+                if (num2 != 0)
+                {
+                    Console.WriteLine(num1 + " / " + num2 + " = " + division(num1, num2));
+                }
+                else
+                {
+                    Console.WriteLine("Error, division en cero");
+                }
+                break;
+        }   
+    }
+}
+
+Console.WriteLine("Saliendo...");
+
 
 
 float solicitarNumero()
